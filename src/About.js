002edeeb8f6fdb061ba7c1f70e994e3e7ef01f42
Joanna Wang <joannawang@joannas-mbp.home>
2024-09-03 15:13:19 -0400

    small change

--- src/About.js
+++ src/About.js
@@ -17,7 +17,7 @@ function About() {
                     to the right place! From the Japanese word meaning, "to eat" or たべる, Taberu is our way of
                     sharing our favourite recipes and food from our cultures with the community. With recipes 
                     from all walks of life, with Taberu, users are always one click away from the perfect afternoon
-                    brunch, family dinner, or picnic appetizer. All that's left to do is cook! 
+                brunch, family dinner, or picnic appetizer. All that's left to do is cook ~ Bon Appetit! 
                 </p>
                 <br/>
                 <p className ={classes.main__container__text}>Link to our <a href="https://thewoksoflife.com/category/how-to/">blog</a> for more cooking tips and tricks</p>
